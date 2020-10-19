e3ca78bb27a841d1d3fc3e6feb964ff2e6d61eb5
VSHEV92 <vshevstev@inbox.ru>
2020-10-19 20:34:06 +0300

    проверил на мониторе работу vga контроллера

---
+++ source/IPS/PLL/PLL_40MHz_inst.v
@@ -0,0 +1,4 @@
+PLL_40MHz	PLL_40MHz_inst (
+	.inclk0 ( inclk0_sig ),
+	.c0 ( c0_sig )
+	);
--- source/verilog/Tik_Tac_Toe.v
+++ source/verilog/Tik_Tac_Toe.v
@@ -1,21 +1,58 @@
 `timescale 1ns/1ps
 
 module Tik_Tac_Toe(
-	input	 clk,
-	input	[15:0] address,
-	output q
+	input	 CLK,
+	input	 RESET_N,
+	output VGA_R,
+	output VGA_G,
+	output VGA_B,
+	output VGA_HSYNC,
+	output VGA_VSYNC
 );
 	
-test_mem
-#(
-	.DEPTH(20),
-	.FILE_NAME("D:/Tik_Tac_Toe/source/matlab/Test_Imag.txt")
-)
-UUT
-(
-	.CLK(clk),
-	.ADDR(address),
-	.Q(q)
+reg reset;
+reg [1:0] resetn_shift;
+wire [4*9-1:0] control_array;
+wire pixel_value;
+
+wire clk_40MHz;
+
+// PLL для генерации тактового сигнала 40 МГц
+PLL_40MHz PLL_Inst(
+	.inclk0(CLK),
+	.c0(clk_40MHz)
+	);
+
+// двойной триггер для защиты от метастабильности
+always @(posedge clk_40MHz)
+begin
+	resetn_shift <= {resetn_shift[0], RESET_N};
+	reset <= ~resetn_shift[1];
+end	
+	
+// ----------------------------------------------------------------------	
+//assign control_array = 36'h000000000; 
+
+VGAC_Test_Source u0 (
+	.source     (control_array),
+	.source_clk (clk_40MHz)
 );
+// ----------------------------------------------------------------------	
+
+
+// контроллер управления монитором 	
+VGA_Controller VGA_Controller_Inst(
+	.CLK(clk_40MHz),
+	.RESET(reset),
+	.CONTROL_ARRAY(control_array),
+	.PIXEL_VALUE(pixel_value),
+	.PIXEL_VALID(),
+	.HSYNC(VGA_HSYNC),
+	.VSYNC(VGA_VSYNC)
+);
+
+assign VGA_R = pixel_value;
+assign VGA_G = pixel_value;
+assign VGA_B = pixel_value;
 	
 endmodule
